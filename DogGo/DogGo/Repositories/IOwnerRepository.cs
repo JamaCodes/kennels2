2d8077119788f91c933d689bd09ed058c1e73bea
JamaCodes <jama.avwork@gmail.com>
2021-12-13 14:47:14 -0600

    first commit

---
+++ DogGo/DogGo/Repositories/IOwnerRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using DogGo.Models;
+using System.Data.SqlClient;
+
+namespace DogGo.Repositories
+{
+    public interface IOwnerRepository
+    {
+        List<Owner> GetAllOwners();
+        Owner GetOwnerById(int id);
+    }
+}
